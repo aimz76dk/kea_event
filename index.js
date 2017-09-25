7b0ff5287031b1113a9d58b97ba5b37fea366cc9
aimz <aimzbooking@gmail.com>
2017-09-25 13:38:00 +0200

    ready for hand-in

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+var path = require('path')
+var bodyParser = require('body-parser');
+const publicPath = __dirname + '/public'
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(express.static('wwwroot'));
+
+app.get('/', function (req, res) {
+    res.sendFile(publicPath + '/login.html', function (err) {
+        // handle error
+    });
+});
+
+app.get('/contact', function (req, res) {
+    res.sendFile(publicPath + '/contact.html', function (err) {
+        // handle error
+    });
+});
+
+app.get('/about', function (req, res) {
+    res.sendFile(publicPath + '/about.html', function (err) {
+        // handle error
+    });
+});
+
+app.get('/login', function(req, res) {
+    res.sendFile(publicPath + '/login.html', function (err) {
+        // handle error
+    });
+});
+
+app.post('/login', function(req, res) {
+     if (req.body.user === 'admin' || req.body.pass === 'dumkode') {
+        res.sendFile(publicPath + '/loggedin.html', function (err) {
+            // handle error
+        });
+    } else {
+        res.json({success: false});
+    }
+});
+
+app.get('/loggedin', function(req, res) {
+    res.sendFile(publicPath + '/loggedin.html', function (err) {
+        // handle error
+    });
+});
+
+app.listen(3000);
